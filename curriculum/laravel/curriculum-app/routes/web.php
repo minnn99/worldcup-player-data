f5db3210c3db72914cced5ddf147756b37fdb845
minn99 <minjae990605@gmail.com>
2025-07-03 23:19:36 +0900

    web.phpコメント修正

--- curriculum/laravel/curriculum-app/routes/web.php
+++ curriculum/laravel/curriculum-app/routes/web.php
@@ -4,19 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayersController;
 use App\Http\Controllers\AuthController;
 
-// Authentication routes
-// 認証関連ルート
+// 認証関連のルート
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [App\Http\Controllers\RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [App\Http\Controllers\RegisterController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Player management routes
-// 選手関連ルート
+// 選手管理のルート
 Route::get('/', [PlayersController::class, 'index'])->name('players.index');
 Route::get('/detail/{id}', [PlayersController::class, 'detail'])->name('players.detail');
-// Ensure only the player ID is passed in the URL and no sensitive data is exposed
+// URLには選手IDのみを渡し、機密情報は公開しないようにする
 Route::get('/edit/{id}', [PlayersController::class, 'edit'])->name('players.edit');
 Route::put('/update/{id}', [PlayersController::class, 'update'])->name('players.update');
 Route::delete('/delete/{id}', [PlayersController::class, 'destroy'])->name('players.destroy');
